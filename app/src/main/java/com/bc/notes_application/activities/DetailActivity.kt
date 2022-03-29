bb96c29ee26c1849de28da174e36aaccff2713d5
unknown <SENA@ARMAPRRQDFSD306.FormacionSena.RED>
2022-03-29 17:49:02 -0500

    Update: detail activity for user registry. BC

--- app/src/main/java/com/bc/notes_application/activities/DetailActivity.kt
+++ app/src/main/java/com/bc/notes_application/activities/DetailActivity.kt
@@ -13,29 +13,54 @@ class DetailActivity : AppCompatActivity() {
 
     var studentId: String? = null
     var student: Student? = null
-    var name: TextView? = null
+    private var title: TextView? = null
+    private var identification: TextView? = null
+    private var name: TextView? = null
+    private var age: TextView? = null
+    private var phoneNumber: TextView? = null
+    private var address: TextView? = null
+    private var subjects: TextView? = null
+    private var grades: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         init()
         studentId = intent.extras?.get("student_id") as String?
-        try {
-            Toast.makeText(this, "Student: ${studentId}", Toast.LENGTH_LONG).show()
-        } catch (e: Exception) {
-            Toast.makeText(this, "No student created", Toast.LENGTH_LONG).show()
-        }
     }
 
     override fun onStart() {
         super.onStart()
-        student = SchoolService.getOne(studentId)
-        //student = students.filter { it.identification == studentId }
-        Toast.makeText(this, "Student found: ${student}", Toast.LENGTH_LONG).show()
-        name?.text = student?.name
+        try {
+            student = SchoolService.getOne(studentId)
+            //var subjects2: String = student?.subjects!!.forEach {
+                //return "${it} "
+            //}
+            var subjects: String = ""
+            for (subject in student!!.subjects) {
+                subjects += "${subject} "
+            }
+            title?.text = "Detalles del estudiante ${student?.name!!.split(' ')[0]}"
+            identification?.text = student?.identification
+            name?.text = student?.name
+            age?.text = student?.age.toString()
+            phoneNumber?.text = student?.phoneNumber
+            address?.text = student?.address
+            this.subjects?.text = subjects
+        } catch (e: Exception) {
+            //Do something
+            Toast.makeText(this, "No pudimos mostrar los datos", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun init() {
+        title = findViewById(R.id.title)
+        identification = findViewById(R.id.identification)
         name = findViewById(R.id.name)
+        age = findViewById(R.id.age)
+        phoneNumber = findViewById(R.id.phone_number)
+        address = findViewById(R.id.address)
+        subjects = findViewById(R.id.subjects)
+        grades = findViewById(R.id.grades)
     }
 }
